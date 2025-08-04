699f49e632c6c9dbd5706da37f93f726cdfd5d79
mohamed hamed <you@example.com>
2025-08-04 11:00:36 +0300

    srpp_id

--- px_custom_journal_entries/models/inherit.py
+++ px_custom_journal_entries/models/inherit.py
@@ -8,3 +8,7 @@ class ResConfigSettings(models.TransientModel):
         ('quarterly', 'Quarterly'),
     ], string="Tax Periodicity")
     account_tax_periodicity_reminder_day = fields.Integer(string="Tax Reminder Day")
+    account_tax_periodicity_journal_id = fields.Many2one(
+        'account.journal',
+        string="Tax Journal"
+    )
